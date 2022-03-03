ae4daeb51998e3ca5e7018cb75fb6844cc29a612
Shubham Chauhan <cshubham1911@gmail.com>
2022-03-03 19:00:15 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Blog from './containers/blog/Blog';
+
+// import { Footer, Blog, Possibility, Features, WhatGPT3, Header } from './containers';
+// import { CTA, Brand, Navbar } from './components';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <h1>
+       GPT-3
+     </h1>
+     <Blog/>
     </div>
   );
 }
---
+++ src/containers/whatGP3/WhatGP3.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './whatGP3.css'
+
+const whatGP3 = () => {
+  return (
+    <div>whatGP3</div>
+  )
+}
+
+export default whatGP3
